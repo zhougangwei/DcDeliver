48fc8ef565163b780522298f125440f3f1709376
zhougangwei <892537848@qq.com>
2017-12-15 18:21:26 +0800

    第一次更新

---
+++ app/src/main/java/com/aihui/dcdeliver/ui/activity/NewWayBillActivity.java
@@ -0,0 +1,23 @@
+package com.aihui.dcdeliver.ui.activity;
+
+import com.aihui.dcdeliver.R;
+import com.aihui.dcdeliver.base.AppActivity;
+
+public class NewWayBillActivity extends AppActivity {
+
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_new_waybill;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
---
+++ app/src/main/java/com/aihui/dcdeliver/ui/fragment/NewWaybillFragment.java
@@ -0,0 +1,50 @@
+package com.aihui.dcdeliver.ui.fragment;
+
+import com.aihui.dcdeliver.R;
+import com.aihui.dcdeliver.base.BaseFragment;
+
+/**
+ * @ 创建者   zhou
+ * @ 创建时间   2017/3/17 15:31
+ * @ 描述    ${TODO}
+ * @ 更新者  $AUTHOR$
+ * @ 更新时间    2017/3/17$
+ * @ 更新描述  ${TODO}
+ */
+
+public class NewWaybillFragment extends BaseFragment<HomeImpl>  {
+
+
+
+
+    @Override
+    protected void initView() {
+
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_new_waybill;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
